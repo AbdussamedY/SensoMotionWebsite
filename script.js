48d2e7db2a064fd3c66185480f86deea54081ac5
AbdussamedY <samedyazici1905@gmail.com>
2024-04-21 21:19:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const membres = document.querySelectorAll(".membre");
+
+    membres.forEach((membre) => {
+        const tooltip = document.createElement("div");
+        tooltip.classList.add("tooltip");
+        tooltip.style.display = "none";
+        tooltip.style.position = "fixed";
+        tooltip.style.backgroundColor = "#333";
+        tooltip.style.color = "#fff";
+        tooltip.style.padding = "5px";
+        tooltip.style.borderRadius = "5px";
+        tooltip.style.zIndex = "1000";
+
+        document.body.appendChild(tooltip); // Attacher l'infobulle au body
+
+        membre.addEventListener("mouseenter", () => {
+            const info = membre.getAttribute("member-info");
+
+            if (info) { // Vérifiez si 'info' n'est pas nul
+                // Vérifiez si 'info' commence et se termine par le signe '$'
+                if (info.startsWith('$') && info.endsWith('$')) {
+                    tooltip.innerHTML = info; // Contenu LaTex
+                    MathJax.typesetPromise([tooltip]); // Rendu LaTex
+                } else {
+                    tooltip.innerHTML = info; // Contenu normal
+                }
+
+                tooltip.style.display = "block"; // Afficher l'infobulle
+            }
+        });
+
+        membre.addEventListener("mousemove", (event) => {
+            const offset = 10;
+            tooltip.style.top = `${event.clientY + offset}px`;
+            tooltip.style.left = `${event.clientX + offset}px`;
+        });
+
+        membre.addEventListener("mouseleave", () => {
+            tooltip.style.display = "none";
+        });
+    });
+});
